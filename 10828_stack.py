1a154bfb1d0e85e7d2a3b2413a3711c1fd88760e
joonwoo-yoo <dbwnsdn122@naver.com>
2026-04-10 14:08:46 +0900

    [BOJ-10828번] 스택

---
+++ 10828_stack.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+stack = []
+n = int(input())
+
+for _ in range(n):
+    command = input().split()
+
+    if command[0] == "push":
+        stack.append(int(command[1]))
+
+    elif command[0] == "pop":
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+
+    elif command[0] == "size":
+        print(len(stack))
+
+    elif command[0] == "empty":
+        print(0 if stack else 1)
+
+    elif command[0] == "top":
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
